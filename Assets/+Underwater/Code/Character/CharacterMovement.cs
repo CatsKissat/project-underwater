ce651a48db83787df0ee30acab3409ad5a5095ac
J-M J <Juha-matti.jarvi@tuni.fi>
2022-10-02 23:07:24 +0300

    namespace correction
    changed namespace to FlamingApes.uinderwater, added twin stick controller and keyboard + mouse controls

--- Assets/+Underwater/Code/Character/CharacterMovement.cs
+++ Assets/+Underwater/Code/Character/CharacterMovement.cs
@@ -8,38 +8,72 @@ namespace FlamingApes.Underwater
     public class CharacterMovement : MonoBehaviour
     {
 
-        //references for rotation for aiming at mouse
-        public new Rigidbody2D rigidbody2D;
-        public Camera main;
-        Vector2 mousePosWorld;
-        Vector3 mousePosCurrent;
-
-        //player movement references
-        private Vector2 input;
+        //references for rotation for aiming at mouse and keyboard
+        [SerializeField]
+        private Rigidbody2D rb2D;
 
         [SerializeField]
-        float speed = 1;
+        private Camera main;
+
+        [SerializeField] 
+        private bool isGamepadActive;
+        
+        private Vector2 mousePositionWorld;
+        private Vector3 mousePositionCurrent;
+
+        //player movement references
+       
+        private Vector2 input;
+        
+
+        [SerializeField]
+        float movementSpeed = 1;
 
         // Update is called once per frame
-        //Character moving with new unity input system, Space.World is for letting the rotation to affect movement direction
+        
         void Update()
-        {  
-            transform.Translate(input * speed * Time.deltaTime, Space.World);
+        { 
+            
+
+            //Moves character, without letting the rotation to affect movement direction
+            transform.Translate(input * movementSpeed * Time.deltaTime, Space.World);
+
+            //If gamepad is active and sensing input from stick controller aim with right controller
+            //if (isGamepad && rightStickInput.magnitude > 0.0f)
+            if (isGamepadActive && input.magnitude > 0.0f)
+            {
+                //Right stick input reading for rotation
+                var gamepad = Gamepad.current;
+                Vector3 rightStickInput = gamepad.rightStick.ReadValue();
+
+                float angleToLook = Mathf.Atan2(rightStickInput.y, rightStickInput.x) * Mathf.Rad2Deg - 90f;
+                rb2D.rotation = angleToLook;
+                Debug.Log("Gamepad active");
+            }
+            //if no gamepad active use mouse to aim to mouses current position
+            else
+            {
+                mousePositionCurrent = Mouse.current.position.ReadValue();
+                mousePositionWorld = main.ScreenToWorldPoint(mousePositionCurrent);
+
+                Vector2 lookDirection = mousePositionWorld - rb2D.position;
+                float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+                rb2D.rotation = angle;
+            }
         }
 
+        //input reading for controls
         public void Move(InputAction.CallbackContext context)
         {
             input = context.ReadValue<Vector2>();
+                
         }
 
-        //rotating the character towards the point of the mouse in the world by rotating characters rigidbody2D
-        private void FixedUpdate()
+        //Unity event OnDeviceChange reads the boolean if controller is equiped
+        public void OnDeviceChange(PlayerInput controllerInput)
         {
-            mousePosCurrent = Mouse.current.position.ReadValue();
-            mousePosWorld = Camera.main.ScreenToWorldPoint(mousePosCurrent);
-            Vector2 lookDir = mousePosWorld - rigidbody2D.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-            rigidbody2D.rotation = angle;
+            isGamepadActive = controllerInput.currentControlScheme.Equals("Gamepad") ? true : false;
         }
     }
 }
