14de2e3fcc3b0541c7a4704bb13f8091764519ba
J-M J <Juha-matti.jarvi@tuni.fi>
2022-10-09 14:45:33 +0300

    PlayerAttack with object pooling 1.0
    Added player attack, projectile prefab and object pooling code

---
+++ Assets/+Underwater/Code/Character/PlayerAttack.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FlamingApes.Underwater
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+        //shooting related references solely for PlayerCharacter
+
+        public GameObject projectile;
+
+        //firePoint is empty gameobject, which is placed in parent object and works as point of bullets origin
+        public Transform firePoint;
+        //boolean for shooting cooldown and amount cooldown is active
+        private bool canAttack = true;
+
+        [SerializeField]
+        float shootingCooldown = 1f;
+
+
+        //After CanShoot() numerator sends positive boolean, Shoot() calls bullet pool to set prefab active / visible 
+        public void Shoot(InputAction.CallbackContext context)
+        {
+            if (!canAttack) return;
+            //fetch pooled game object -> the projectile in this case
+            GameObject projectile = ObjectPool.SharedInstance.GetPooledObject();
+            if (projectile != null)
+            {
+                projectile.transform.position = firePoint.transform.position;
+                projectile.transform.rotation = firePoint.transform.rotation;
+                projectile.SetActive(true);
+                Debug.Log("FIRE!");
+            }
+
+            StartCoroutine(CanShoot());
+
+        }
+
+        //cooldown for shooting 
+        IEnumerator CanShoot()
+        {
+            canAttack = false;
+            yield return new WaitForSeconds(shootingCooldown);
+            canAttack = true;
+        }
+    }
+}
