1bf1722eb1a3e7c40dc0b02f67288b8c94b5e45d
J-M J <Juha-matti.jarvi@tuni.fi>
2022-11-03 13:07:18 +0200

    reference fix on Camera.main to just main

--- Assets/+Underwater/Code/Character/CharacterMovement.cs
+++ Assets/+Underwater/Code/Character/CharacterMovement.cs
@@ -10,7 +10,7 @@ namespace FlamingApes.Underwater
 
         //references for rotation for aiming at mouse and keyboard
         [SerializeField]
-        private Camera main;
+        private Camera mainCamera;
 
         [SerializeField]
         private bool isGamepadActive;
@@ -75,8 +75,8 @@ namespace FlamingApes.Underwater
         private Vector2 GetMousePointerInput()
         {
             Vector3 mousePos = pointerPosition.action.ReadValue<Vector2>();
-            mousePos.z = Camera.main.nearClipPlane;
-            return Camera.main.ScreenToWorldPoint(mousePos);
+            mousePos.z = mainCamera.nearClipPlane;
+            return mainCamera.ScreenToWorldPoint(mousePos);
         } 
 
     //input reading for controls
