ff6ce8923e542f39400f4ca1b7139044b71f731e
J-M J <Juha-matti.jarvi@tuni.fi>
2022-11-22 17:04:15 +0200

    Damage fix
    Removed unitSensor and excessive colliders, now only inflictDamage.cs is needed to do damage to player/enemies

--- Assets/+Underwater/Code/Character/InflictDamage.cs
+++ Assets/+Underwater/Code/Character/InflictDamage.cs
@@ -20,13 +20,12 @@ namespace FlamingApes.Underwater
         [SerializeField]
         private int damage = 1;
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
+            IHealth health = collision.gameObject.GetComponentInChildren<IHealth>();
+            UnitBase targetUnit = collision.gameObject.GetComponentInChildren<UnitBase>();
             InflictDamage player = GetComponent<InflictDamage>();
             InflictDamage enemy = collision.gameObject.GetComponent<InflictDamage>();
-
-            IHealth health = collision.GetComponentInChildren<IHealth>();
-            UnitBase targetUnit = collision.GetComponentInChildren<UnitBase>();
 
             // do both objects in this collision actually have a faction membership? 
             // If not, then we are not interested in this collision.
@@ -35,21 +34,20 @@ namespace FlamingApes.Underwater
                 return;
             }
             // check if we have encountered an enemy
-            if (player.faction != enemy.faction)
+            if (player.faction != enemy.faction && health != null)
             {
-                if (health != null)
+
+                if (!health.DecreaseHealth(damage))
                 {
-                    if (!health.DecreaseHealth(damage))
+                    // Target died
+                    if (targetUnit != null)
                     {
-                        // Target died
-                        if (targetUnit != null)
-                        {
-                            // TODO: Character should have a death effect and transitioning to GameOver should be delayed
-                            targetUnit.Die();
-                        }
+                        // TODO: Character should have a death effect and transitioning to GameOver should be delayed
+                        targetUnit.Die();
                     }
                 }
             }
+
         }
     }
 }
