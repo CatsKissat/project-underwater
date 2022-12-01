b7f92cde55e829a69908025d652d3da49c343c4e
J-M J <Juha-matti.jarvi@tuni.fi>
2022-12-01 12:07:27 +0200

    Projectile code fixed

--- Assets/+Underwater/Code/Character/Projectile.cs
+++ Assets/+Underwater/Code/Character/Projectile.cs
@@ -27,7 +27,12 @@ namespace FlamingApes.Underwater
             gameObject.SetActive(false);
         }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             gameObject.SetActive(false);
         }
